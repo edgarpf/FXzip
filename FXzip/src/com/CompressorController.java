2f899cde0cd30f8acfd2b43a988aff4b6f2c3f8f
Edgar <Edgar@edgar>
2015-10-30 16:36:03 -0200

    input/output files dialogs done.

--- FXzip/src/com/CompressorController.java
+++ FXzip/src/com/CompressorController.java
@@ -1,26 +1,67 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
+import javafx.event.*;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javax.swing.JFileChooser;
 
-/**
- *
- * @author Edgar
- */
 public class CompressorController implements Initializable {
-            
+
+    @FXML
+    private ProgressBar pbar;
+
+    @FXML
+    private TextField txtInput;
+    
+    @FXML
+    private TextField txtOutput;
+    
+    @FXML
+    private TextField txtPass;
+
+    @FXML
+    private Button btnStart;
+
+    @FXML
+    private ComboBox<String> cbAction;
+
+    private File input;
+    
+    private File outputDirectory;
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+
+    }
+
+    @FXML
+    public void browseInput(ActionEvent event) {
+        showFileSelectionDialog(input, txtInput, JFileChooser.FILES_AND_DIRECTORIES, 
+                "Choose a file/diretory to compress or a file to decompress");                              
+    }
+
+    @FXML
+    public void browseOutput(ActionEvent event) {
+        showFileSelectionDialog(outputDirectory, txtOutput, JFileChooser.DIRECTORIES_ONLY, 
+                "Choose a diretory");                 
+    }
     
+    private void showFileSelectionDialog(File file, TextField txtPath ,int selectionMode, String dialogTitile)
+    {
+        JFileChooser chooser = new JFileChooser(".");       
+        chooser.setDialogTitle(dialogTitile);
+        chooser.setFileSelectionMode(selectionMode);
+        int ret = chooser.showOpenDialog(null);        
+        if (ret == JFileChooser.APPROVE_OPTION) {
+            file = chooser.getSelectedFile();  
+            txtPath.setText(file.getPath());
+        }  
+        
+        else
+            file = null;
+    }
 }
--- FXzip/src/com/Main.java
+++ FXzip/src/com/Main.java
@@ -1,21 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.scene.*;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-/**
- *
- * @author Edgar
- */
 public class Main extends Application {
     
     @Override
@@ -29,10 +19,7 @@ public class Main extends Application {
         stage.setResizable(false);
         stage.show();
     }
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         launch(args);
     }
