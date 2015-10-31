b608343233700ca39b4343e6fdcc625f87b8ef51
Edgar <Edgar@edgar>
2015-10-31 15:10:35 -0200

    decompress done.

--- FXzip/src/com/FXzip/CompressorController.java
+++ FXzip/src/com/FXzip/CompressorController.java
@@ -2,12 +2,18 @@ package com.FXzip;
 
 import com.FXzip.util.Util;
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 import javafx.event.*;
 import javafx.fxml.*;
 import javafx.scene.control.*;
 import javax.swing.JFileChooser;
+import net.lingala.zip4j.core.ZipFile;
+import net.lingala.zip4j.exception.ZipException;
+import net.lingala.zip4j.model.FileHeader;
+import org.apache.commons.io.FilenameUtils;
 
 public class CompressorController implements Initializable {
 
@@ -16,10 +22,10 @@ public class CompressorController implements Initializable {
 
     @FXML
     private TextField txtInput;
-    
+
     @FXML
     private TextField txtOutput;
-    
+
     @FXML
     private TextField txtPass;
 
@@ -30,9 +36,9 @@ public class CompressorController implements Initializable {
     private ComboBox<String> cbAction;
 
     private File input;
-    
+
     private File outputDirectory;
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
 
@@ -40,53 +46,79 @@ public class CompressorController implements Initializable {
 
     @FXML
     public void browseInput(ActionEvent event) {
-        input = showFileSelectionDialog(txtInput, JFileChooser.FILES_AND_DIRECTORIES, 
-                "Choose a file/diretory to compress or a file to decompress");                              
+        input = showFileSelectionDialog(txtInput, JFileChooser.FILES_AND_DIRECTORIES,
+                "Choose a file/diretory to compress or a file to decompress");
     }
 
     @FXML
     public void browseOutput(ActionEvent event) {
-        outputDirectory = showFileSelectionDialog(txtOutput, JFileChooser.DIRECTORIES_ONLY, 
-                "Choose a diretory");                 
+        outputDirectory = showFileSelectionDialog(txtOutput, JFileChooser.DIRECTORIES_ONLY,
+                "Choose a diretory");
     }
-    
-    private File showFileSelectionDialog(TextField txtPath ,int selectionMode, String dialogTitile)
-    {
+
+    private File showFileSelectionDialog(TextField txtPath, int selectionMode, String dialogTitile) {
         File file = null;
-        JFileChooser chooser = new JFileChooser(".");       
+        JFileChooser chooser = new JFileChooser(".");
         chooser.setDialogTitle(dialogTitile);
         chooser.setFileSelectionMode(selectionMode);
-        int ret = chooser.showOpenDialog(null);        
+        int ret = chooser.showOpenDialog(null);
         if (ret == JFileChooser.APPROVE_OPTION) {
-            file = chooser.getSelectedFile();  
-            txtPath.setText(file.getPath());            
-        }  
-        
+            file = chooser.getSelectedFile();
+            txtPath.setText(file.getPath());
+        }
+
         return file;
     }
-    
+
     @FXML
-    public void start()
-    {
-        if(input == null || outputDirectory == null)
-        {
-            Util.showErrorDialog("Error", "File Error", "You need to insert a valid path to input and output fields.");            
+    public void start() throws IOException {
+        if (input == null || outputDirectory == null) {
+            Util.showErrorDialog("Error", "File Error", "You need to insert a valid path in input and output fields.");
             return;
         }
-        
-        if(cbAction.getSelectionModel().getSelectedItem().equals("Compress"))
-            compress();
-        else
+
+        String action = cbAction.getSelectionModel().getSelectedItem();
+
+        if (action.equalsIgnoreCase("Decompress") && !FilenameUtils.getExtension(input.getName()).equals("zip")) {
+            Util.showErrorDialog("Error", "File Error", "You need to insert a zip file in input.");
+            return;
+        }
+
+        if (action.equals("Decompress")) {
             decompress();
+        } else {
+            compress();
+        }
     }
-    
-    private void compress()
-    {
-        
+
+    private void compress() {
+
     }
-    
-    private void decompress()
-    {
-        
+
+    private void decompress() throws IOException {
+        try {
+
+            ZipFile zipFile = new ZipFile(input.getPath());
+            
+            if (zipFile.isEncrypted()) {
+                if (txtPass.getText().equals("")) {
+                    zipFile.setPassword("1");
+                } else {
+                    zipFile.setPassword(txtPass.getText());
+                }
+
+            }
+
+
+            List fileHeaderList = zipFile.getFileHeaders();
+
+            for (int i = 0; i < fileHeaderList.size(); i++) {
+                FileHeader fileHeader = (FileHeader) fileHeaderList.get(i);
+                zipFile.extractFile(fileHeader, outputDirectory.getPath());
+            }
+                        
+        } catch (ZipException ex) {
+            Util.showErrorDialog("Error", "File Error", ex.getMessage());
+        }
     }
 }
--- FXzip/src/com/FXzip/util/Util.java
+++ FXzip/src/com/FXzip/util/Util.java
@@ -1,14 +1,17 @@
 package com.FXzip.util;
 
 import javafx.scene.control.Alert;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
 
 public class Util {
 
     public static void showErrorDialog(String title, String headerText, String message) {
-        Alert dialogoErro = new Alert(Alert.AlertType.ERROR);
-        dialogoErro.setTitle(title);
-        dialogoErro.setHeaderText(headerText);
-        dialogoErro.setContentText(message);
-        dialogoErro.showAndWait();
+        Alert dialog = new Alert(Alert.AlertType.ERROR);
+        dialog.setTitle(title);        
+        dialog.setHeaderText(headerText);
+        dialog.setContentText(message);
+        ((Stage)dialog.getDialogPane().getScene().getWindow()).getIcons().add(new Image("icon.png"));
+        dialog.showAndWait();
     }
 }
