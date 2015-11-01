0a60f3e253c631a60e83029077963c3cac5b76ff
Edgar <Edgar@edgar>
2015-11-01 18:40:56 -0200

    FXzip with zip support.

--- FXzip/src/com/FXzip/util/Util.java
+++ FXzip/src/com/FXzip/util/Util.java
@@ -1,13 +1,14 @@
 package com.FXzip.util;
 
 import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Util {
 
-    public static void showErrorDialog(String title, String headerText, String message) {
-        Alert dialog = new Alert(Alert.AlertType.ERROR);
+    public static void showErrorDialog(String title, String headerText, String message, AlertType alertType) {
+        Alert dialog = new Alert(alertType);
         dialog.setTitle(title);        
         dialog.setHeaderText(headerText);
         dialog.setContentText(message);
